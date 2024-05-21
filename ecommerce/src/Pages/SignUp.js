4b1545d3606050af20e48e74df2c6112645fde34
ehthasham <mohammedehthasham@gmail.com>
2024-05-21 21:18:36 +0530

    new123

--- ecommerce/src/Pages/SignUp.js
+++ ecommerce/src/Pages/SignUp.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div className='text-red-600 text-5xl m-72'>SignUp</div>
+    <div className='text-red-600 text-5xl m-72'>SignUp kiraak</div>
   )
 }
 
